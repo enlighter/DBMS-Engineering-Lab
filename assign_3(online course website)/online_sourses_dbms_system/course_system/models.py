bc158bb0470e9329e0b58cbcc9414972211c49dc
enlighter <mandal.sushovan92@gmail.com>
2016-03-01 15:14:08 +0530

    complete description of all entity models for the db

--- assign_3(online course website)/online_sourses_dbms_system/course_system/models.py
+++ assign_3(online course website)/online_sourses_dbms_system/course_system/models.py
@@ -11,14 +11,30 @@ class learners(models.Model):
     #set password in views.py userform
 
 class course(models.Model):
-    coursename = models.CharField(max_length=42)
+    course_name = models.CharField(max_length=42)
     courseID = models.AutoField(primary_key=True)
     about = models.CharField(max_length=999)
     provided_by_institute = models.CharField(max_length=99, default="freelance", blank=True)
     domain = models.CharField(max_length=33)
 
 
+class instructor(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=60, unique=True)
+    website = models.TextField(validators=[URLValidator()], null=True, blank=True)
+    instructorID = models.AutoField(primary_key=True)
 
-#class instructor(models.Model):
-#    firstname = models.CharField(max_length=30)
-#    lastname = models.CharField(max_length=30)
+
+class course_content(models.Model):
+    courseID = models.ForeignKey(course, on_delete=models.CASCADE)
+    partID = models.AutoField(primary_key=True)
+    content_location = models.TextField(validators=[URLValidator()])
+    content_name = models.CharField(max_length=30)
+    content_type = models.CharField(max_length=18)
+    has_evaluation = models.BooleanField()
+
+
+class performance(models.Model):
+    score = models.TextField(max_length=10)
+    partID = models.ForeignKey(course_content, on_delete=models.CASCADE)
