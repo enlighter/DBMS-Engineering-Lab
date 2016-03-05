ea2278ae4500fa7a9fca9cafe4d869f19e58f122
enlighter <mandal.sushovan92@gmail.com>
2016-03-05 17:00:21 +0530

    correct csrf request error for login page

--- assign_3(online course website)/online_sourses_dbms_system/course_system/views.py
+++ assign_3(online course website)/online_sourses_dbms_system/course_system/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, render_to_response
-from django.http import HttpResponse
+from django.http import *
 from django.template import loader, RequestContext
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
@@ -14,8 +14,18 @@ def index(request):
 
 
 def register_student(request):
-    template = loader.get_template('course_system/index.html')
+    logout(request)
+    username = password = ''
+    if request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('/main/')
+    return render_to_response('course_system/index.html', context_instance=RequestContext(request))
     #context = {
     #    'student_registration': student_registration,
     #}
-    return HttpResponse(template.render(request))
--- assign_3(online course website)/online_sourses_dbms_system/online_sourses_dbms_system/settings.py
+++ assign_3(online course website)/online_sourses_dbms_system/online_sourses_dbms_system/settings.py
@@ -41,14 +41,11 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE_CLASSES = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'online_sourses_dbms_system.urls'
