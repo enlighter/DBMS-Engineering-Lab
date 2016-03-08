68653b3dcc517f0f0eec0a2e2cdd6bc21af9d573
enlighter <mandal.sushovan92@gmail.com>
2016-03-08 20:55:52 +0530

    streamline urls implementation

--- assign_3(online course website)/protOC/courseSystem/urls.py
+++ assign_3(online course website)/protOC/courseSystem/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'courseSystem'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^register_student/$', views.reg_student, name="register_student"),
+    url(r'^register_student/$', views.register_student, name="register_student"),
     url(r'^register_teacher/$', views.register_teacher, name="register_teacher")
 ]
--- assign_3(online course website)/protOC/courseSystem/views.py
+++ assign_3(online course website)/protOC/courseSystem/views.py
@@ -14,7 +14,7 @@ def index(request):
     template = loader.get_template('courseSystem/index.html')
     return HttpResponse(template.render(request))
 
-def reg_student(request):
+def register_student(request):
     logout(request)
 
     registered = False
@@ -48,46 +48,6 @@ def reg_student(request):
             'courseSystem/student_login.html',
             {'registered': registered})
 
-
-def register_student(request):
-    logout(request)
-
-    registered = False
-
-    if request.method == 'POST':
-        user_form = UserCreationForm(data=request.POST)
-        learners_profile_form = LearnersProfileForm(data=request.POST)
-
-        if user_form.is_valid() and learners_profile_form.is_valid():
-            user = user_form.save()
-            user.set_password(user.password)
-            user.save()
-
-            profile = learners_profile_form.save(commit=False)
-            profile.user = user
-            profile.save()
-
-            registered = True
-
-        else:
-            print( user_form.errors, learners_profile_form.errors )
-
-    # Not a HTTP POST, so we render our form using two ModelForm instances.
-    # These forms will be blank, ready for user input.
-    else:
-        user_form = UserCreationForm()
-        learners_profile_form = LearnersProfileForm()
-
-        #user = authenticate(username=username, password=password)
-        #if user is not None:
-        #    if user.is_active:
-        #        login(request, user)
-        #        return HttpResponseRedirect('/main/')
-
-    # Render the template depending on the context.
-    return render(request,
-            'courseSystem/student_login.html',
-            {'user_form': user_form, 'profile_form': learners_profile_form, 'registered': registered})
 
 def register_teacher(request):
     logout(request)
