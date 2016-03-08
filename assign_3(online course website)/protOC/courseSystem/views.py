0a2da3efec788c3267ab057ccb5b70598c299c2f
enlighter <mandal.sushovan92@gmail.com>
2016-03-08 22:32:25 +0530

    handle exceptions and reflect them back to the html template pages in views

--- assign_3(online course website)/protOC/courseSystem/views.py
+++ assign_3(online course website)/protOC/courseSystem/views.py
@@ -1,3 +1,5 @@
+import traceback
+from django.db.utils import IntegrityError
 from django.shortcuts import render, redirect, render_to_response
 from django.http import *
 from django.template import loader, RequestContext
@@ -15,69 +17,103 @@ def index(request):
     return HttpResponse(template.render(request))
 
 def register_student(request):
-    logout(request)
+    error = False
+    error_msg = ''
+    try:
+        logout(request)
 
-    registered = False
+        registered = False
 
-    if request.POST :
-        email = request.POST['username']
-        firstname = request.POST['firstname']
-        lastname = request.POST['lastname']
-        nick = request.POST['nick']
-        website = request.POST['website']
-        password1 = request.POST['password']
-        password2 = request.POST['repeat_password']
+        if request.POST :
+            email = request.POST['username']
+            firstname = request.POST['firstname']
+            lastname = request.POST['lastname']
+            nick = request.POST['nick']
+            website = request.POST['website']
+            password1 = request.POST['password']
+            password2 = request.POST['repeat_password']
 
-        if password1 and password2 and password1 != password2:
-            raise forms.ValidationError("Passwords don't match")
-        else:
-            password = password2
+            if password1 and password2 and password1 != password2:
+                raise forms.ValidationError("Passwords don't match")
+            else:
+                password = password2
 
-        user = MyUser(email=email, password=password, firstname=firstname, lastname=lastname,
+            user = MyUser(email=email, password=password, firstname=firstname, lastname=lastname,
                                          website=website)
-        user.save()
-        learner = Learners(user=user, nick=nick)
-        learner.save()
+            user.save()
+            learner = Learners(user=user, nick=nick)
+            learner.save()
 
-        registered = True
+            registered = True
 
-    else:
-        print("POST method of submitting form is required!")
+        else:
+            print("POST method of submitting form is required!")
+
+    except IntegrityError as e:
+        error = True
+        errno, strerror = e.args
+        error_msg = "Encountered error: \nIntegrity error({0}): {1}".format(errno, strerror)
+
+    except forms.ValidationError as e:
+        error = True
+        strerror = e.args
+        error_msg = "Encountered error: \nValidation error: {0}".format(strerror)
+
+    except:
+        error = True
+        error_msg = "Encountered error:" + traceback.format_exc(limit=1)
 
     return render(request,
             'courseSystem/student_login.html',
-            {'registered': registered})
+            {'registered': registered, 'error': error, 'error_msg': error_msg})
 
 
 def register_teacher(request):
-    logout(request)
+    error = False
+    error_msg = ''
+    try:
+        logout(request)
 
-    registered = False
+        registered = False
 
-    if request.POST :
-        email = request.POST['username']
-        firstname = request.POST['firstname']
-        lastname = request.POST['lastname']
-        website = request.POST['website']
-        password1 = request.POST['password']
-        password2 = request.POST['repeat_password']
+        if request.POST :
+            email = request.POST['username']
+            firstname = request.POST['firstname']
+            lastname = request.POST['lastname']
+            website = request.POST['website']
+            password1 = request.POST['password']
+            password2 = request.POST['repeat_password']
 
-        if password1 and password2 and password1 != password2:
-            raise forms.ValidationError("Passwords don't match")
-        else:
-            password = password2
+            if password1 and password2 and password1 != password2:
+                raise forms.ValidationError("Passwords don't match")
+            else:
+                password = password2
 
-        user = MyUser(email=email, password=password, firstname=firstname, lastname=lastname,
+            user = MyUser(email=email, password=password, firstname=firstname, lastname=lastname,
                                          website=website)
-        user.save()
-        teacher = Instructor(user=user)
-        teacher.save()
+            user.save()
+            teacher = Instructor(user=user)
+            teacher.save()
 
-        registered = True
+            registered = True
 
-    else:
-        print("POST method of submitting form is required!")
+        else:
+            print("POST method of submitting form is required!")
+
+    except IntegrityError as e:
+        error = True
+        errno, strerror = e.args
+        error_msg = "Encountered error: \nIntegrity error({0}): {1}".format(errno, strerror)
+
+    except forms.ValidationError as e:
+        error = True
+        strerror = e.args
+        error_msg = "Encountered error: \nValidation error: {0}".format(strerror)
+
+    except:
+        error = True
+        error_msg = "Encountered error:" + traceback.format_exc(limit=1)
 
     return render(request,
             'courseSystem/teacher_login.html',
-            {'registered': registered})
+            {'registered': registered, 'error': error, 'error_msg': error_msg})
