2511dcd5e0b39887dc5b6106f097638140957c35
enlighter <mandal.sushovan92@gmail.com>
2016-03-08 21:36:55 +0530

    do the proper implementation of teacher login view

--- assign_3(online course website)/protOC/courseSystem/views.py
+++ assign_3(online course website)/protOC/courseSystem/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django import forms
 
-from .models import MyUser, Learners
+from .models import MyUser, Learners, Instructor
 from .admin import UserCreationForm, UserChangeForm
 from .forms import LearnersProfileForm
 
@@ -51,14 +51,33 @@ def register_student(request):
 
 def register_teacher(request):
     logout(request)
-    username = password = ''
-    if request.POST:
-        username = request.POST['username']
-        password = request.POST['password']
 
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            if user.is_active:
-                login(request, user)
-                return HttpResponseRedirect('/main/')
-    return render_to_response('courseSystem/teacher_login.html', context_instance=RequestContext(request))
+    registered = False
+
+    if request.POST :
+        email = request.POST['username']
+        firstname = request.POST['firstname']
+        lastname = request.POST['lastname']
+        website = request.POST['website']
+        password1 = request.POST['password']
+        password2 = request.POST['repeat_password']
+
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        else:
+            password = password2
+
+        user = MyUser(email=email, password=password, firstname=firstname, lastname=lastname,
+                                         website=website)
+        user.save()
+        teacher = Instructor(user=user)
+        teacher.save()
+
+        registered = True
+
+    else:
+        print("POST method of submitting form is required!")
+
+    return render(request,
+            'courseSystem/teacher_login.html',
+            {'registered': registered})
