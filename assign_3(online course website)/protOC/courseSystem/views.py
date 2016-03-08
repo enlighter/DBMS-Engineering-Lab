be3edfea7d2ca18ed1e57c6a608c1269dc8bc395
enlighter <mandal.sushovan92@gmail.com>
2016-03-08 20:52:34 +0530

    change the django registration integration implementation and get a working register page

--- assign_3(online course website)/protOC/courseSystem/views.py
+++ assign_3(online course website)/protOC/courseSystem/views.py
@@ -3,15 +3,50 @@ from django.http import *
 from django.template import loader, RequestContext
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django import forms
 
-from .models import Learners
+from .models import MyUser, Learners
 from .admin import UserCreationForm, UserChangeForm
-from .forms import *
+from .forms import LearnersProfileForm
 
 
 def index(request):
     template = loader.get_template('courseSystem/index.html')
     return HttpResponse(template.render(request))
+
+def reg_student(request):
+    logout(request)
+
+    registered = False
+
+    if request.POST :
+        email = request.POST['username']
+        firstname = request.POST['firstname']
+        lastname = request.POST['lastname']
+        nick = request.POST['nick']
+        website = request.POST['website']
+        password1 = request.POST['password']
+        password2 = request.POST['repeat_password']
+
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        else:
+            password = password2
+
+        user = MyUser(email=email, password=password, firstname=firstname, lastname=lastname,
+                                         website=website)
+        user.save()
+        learner = Learners(user=user, nick=nick)
+        learner.save()
+
+        registered = True
+
+    else:
+        print("POST method of submitting form is required!")
+
+    return render(request,
+            'courseSystem/student_login.html',
+            {'registered': registered})
 
 
 def register_student(request):
