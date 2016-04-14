f9476a30304134c6f62c4a259e418b58954a53d9
Sushovan Mandal <mandal.sushovan92@gmail.com>
2016-04-14 23:12:18 +0530

    add reducer script

---
+++ assign_4(Hadoop MapReduce project)/code/tester/reducer.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import sys
+
+def reducer():
+    salesTotal = 0
+    oldKey = None
+
+    for line in sys.stdin:
+        data = line.strip().split("\t")
+
+        #handle fringe cases
+        if len(data) != 2:
+            continue
+
+        thisKey, thisSale = data
+
+        if oldKey and oldKey != thisKey:
+            print( "{0}\t{1}".format(oldKey, salesTotal) )
+            salesTotal = 0
+
+        oldKey = thisKey
+        salesTotal += float(thisSale)
+
+    if oldKey != None:
+        print( "{0}\t{1}".format(oldKey, salesTotal) )
+
+reducer()
