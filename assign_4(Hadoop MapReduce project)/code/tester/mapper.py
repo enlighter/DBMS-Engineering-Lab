7089ef65c0f8156216261411873ceb59651afa32
Sushovan Mandal <mandal.sushovan92@gmail.com>
2016-04-14 21:55:32 +0530

    add defense in mapper against unexpected lines in sales data

--- assign_4(Hadoop MapReduce project)/code/tester/mapper.py
+++ assign_4(Hadoop MapReduce project)/code/tester/mapper.py
@@ -1,6 +1,11 @@
+import sys
+
 def mapper():
     for line in open('sales_data'):
         data = line.strip().split("\t")
+        if len(data) != 6:
+            print("Unexpected line encountered", file=sys.stderr)
+            continue
         date, time, store, item, cost, payment = data
         print( "{0}\t{1}".format(store, cost) )
 
