4be15431d305bef34f45a74f7bc3ffd1887ceefb
Sushovan Mandal <mandal.sushovan92@gmail.com>
2016-04-15 11:07:53 +0530

    run the hadoop map reduce job on whole purchases.txt data

--- assign_4(Hadoop_MapReduce_project)/code/tester/hadoop-operations.sh
+++ assign_4(Hadoop_MapReduce_project)/code/tester/hadoop-operations.sh
@@ -6,4 +6,5 @@ reducerlink="$currentdir/reducer.py"
 #sales_data and mapper and reducer doesn't have any 'blank space in it'
 hadoop fs -put sales_data
 hadoop jar /usr/lib/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.7.2.jar -mapper mapper.py -reducer reducer.py -file mapper.py -file reducer.py -input sales_data -output salesout
+hadoop jar /usr/lib/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.7.2.jar -mapper mapper.py -reducer reducer.py -file mapper.py -file reducer.py -input purchases.txt -output salesout2
 hadoop fs -put access_log
