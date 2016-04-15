717ed3827e4970b06245674bcbb204fadb5b343f
Sushovan Mandal <mandal.sushovan92@gmail.com>
2016-04-15 11:55:20 +0530

    create new reducer to find max value for a key

---
+++ assign_4(Hadoop_MapReduce_project)/code/tester/sales_reducer_storeMax.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import sys
+
+def reducer():
+    salesTotal = 0
+    oldKey = None
+
+    for line in sys.stdin:
+        data = line.strip().split("\t")
+
+        #handle fringe cases
+        if len(data) != 2:
+            #print("Unexpected line encountered with data = ",data, file=sys.stderr)
+            continue
+
+        thisKey, thisSale = data
+
+        if oldKey and oldKey != thisKey:
+            print( "{0}\t{1}".format(oldKey, salesTotal) )
+            salesTotal = 0
+
+        oldKey = thisKey
+        if float(thisSale) > salesTotal:
+            salesTotal = float(thisSale)
+
+    if oldKey != None:
+        print( "{0}\t{1}".format(oldKey, salesTotal) )
+
+reducer()
