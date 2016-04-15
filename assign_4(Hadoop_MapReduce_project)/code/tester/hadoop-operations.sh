b065f70f0bfb0184447da4ecf051c3999aef36ba
Sushovan Mandal <mandal.sushovan92@gmail.com>
2016-04-15 11:22:36 +0530

    segregate by function

--- assign_4(Hadoop_MapReduce_project)/code/tester/hadoop-operations.sh
+++ assign_4(Hadoop_MapReduce_project)/code/tester/hadoop-operations.sh
@@ -5,6 +5,6 @@ reducerlink="$currentdir/reducer.py"
 #make sure the abosukte path to the directory containing
 #sales_data and mapper and reducer doesn't have any 'blank space in it'
 hadoop fs -put sales_data
-hadoop jar /usr/lib/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.7.2.jar -mapper mapper.py -reducer reducer.py -file mapper.py -file reducer.py -input sales_data -output salesout
-hadoop jar /usr/lib/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.7.2.jar -mapper mapper.py -reducer reducer.py -file mapper.py -file reducer.py -input purchases.txt -output salesout2
+hadoop jar /usr/lib/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.7.2.jar -mapper sales_mapper_byStore.py -reducer sales_reducer_byStore.py -file sales_mapper_byStore.py -file sales_reducer_byStore.py -input sales_data -output salesout
+hadoop jar /usr/lib/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.7.2.jar -mapper sales_mapper_byStore.py -reducer sales_reducer_byStore.py -file sales_mapper_byStore.py -file sales_reducer_byStore.py -input purchases.txt -output salesout2
 hadoop fs -put access_log
