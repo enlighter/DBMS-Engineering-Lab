36b68b57b939b7edef4e86f2d458b16614dfba97
Sushovan Mandal <mandal.sushovan92@gmail.com>
2016-04-15 14:12:12 +0530

    create shell script for initializing hadoop env

---
+++ assign_4(Hadoop_MapReduce_project)/code/start_hadoop_archlinux.sh
@@ -0,0 +1,5 @@
+sudo systemctl start hadoop-datanode
+sudo systemctl start hadoop-jobtracker
+sudo systemctl start hadoop-namenode
+sudo systemctl start hadoop-secondarynamenode
+sudo systemctl start hadoop-tasktracker
