40838969114f314c249a4f4259ffce69009454c6
Sushovan Mandal <mandal.sushovan92@gmail.com>
2016-04-15 12:19:25 +0530

    do hadoop map red for overall sales

--- assign_4(Hadoop_MapReduce_project)/code/tester/hadoop-operations.sh
+++ assign_4(Hadoop_MapReduce_project)/code/tester/hadoop-operations.sh
@@ -9,4 +9,5 @@ hadoop jar /usr/lib/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.7.2.jar -ma
 hadoop jar /usr/lib/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.7.2.jar -mapper sales_mapper_byStore.py -reducer sales_reducer_byStore.py -file sales_mapper_byStore.py -file sales_reducer_byStore.py -input purchases.txt -output salesout2
 hadoop jar /usr/lib/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.7.2.jar -mapper sales_mapper_byCategory.py -reducer sales_reducer_byCategory.py -file sales_mapper_byCategory.py -file sales_reducer_byCategory.py -input purchases.txt -output sales_by_category
 hadoop jar /usr/lib/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.7.2.jar -mapper sales_mapper_byStore.py -reducer sales_reducer_storeMax.py -file sales_mapper_byStore.py -file sales_reducer_storeMax.py -input purchases.txt -output sales_store_max
+hadoop jar /usr/lib/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.7.2.jar -mapper sales_mapper_byStore.py -reducer sales_reducer_overallSales.py -file sales_mapper_byStore.py -file sales_reducer_overallSales.py -input purchases.txt -output sales_overall
 hadoop fs -put access_log
