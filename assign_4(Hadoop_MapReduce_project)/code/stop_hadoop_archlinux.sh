d163958ac66838530b69b1ebb47b1407503b3ba7
Sushovan Mandal <mandal.sushovan92@gmail.com>
2016-04-17 22:28:41 +0530

    add the stop hadoop script

---
+++ assign_4(Hadoop_MapReduce_project)/code/stop_hadoop_archlinux.sh
@@ -0,0 +1,5 @@
+sudo systemctl stop hadoop-datanode
+sudo systemctl stop hadoop-jobtracker
+sudo systemctl stop hadoop-namenode
+sudo systemctl stop hadoop-secondarynamenode
+sudo systemctl stop hadoop-tasktracker
